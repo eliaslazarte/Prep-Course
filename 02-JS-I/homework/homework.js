d53ed99fe726c613c7efeee1370f84d214ae20c7
eliaslazarte <eliaslazarte_97@hotmail.com>
2022-03-30 21:33:25 -0300

    bash functioning

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "lalaldsadas23";
+const nuevaString = "logre que ande el git bash";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
